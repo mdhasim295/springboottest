3c786741434fc3bfa8266d755a48d9108986fa2d
Hasim <Hasim@Hasim>
2022-10-08 17:10:28 +0530

    WH-8520- First Time file update

---
+++ src/main/java/com/hasim/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.hasim;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Spring boot app");
+	}
+
+}
---
+++ src/test/java/com/hasim/SpringboottestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
